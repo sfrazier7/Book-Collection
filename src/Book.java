6f44939db78a7838a77571f327df84cfbca5309e
sfrazier7 <sfrazier793@gmail.com>
2022-03-21 10:26:16 -0400

    im making a new repository

---
+++ src/Book.java
@@ -0,0 +1,69 @@
+import java.io.Serializable;
+
+public class Book implements Serializable {
+	
+	//Attributes
+	private String title;
+	private String author;
+	private long isbn;
+	private double price;
+	private boolean isBorrowed;
+	
+	//Constructor
+	Book(String title, String author, long isbn, double price, boolean isBorrowed){
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.price = price;
+		this.isBorrowed = isBorrowed;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isBorrowed() {
+		return isBorrowed;
+	}
+
+	public void setBorrowed(boolean isBorrowed) {
+		this.isBorrowed = isBorrowed;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn + ", price=" + price + ", isBorrowed="
+				+ isBorrowed + "]";
+	}
+	
+	
+
+}
---
+++ src/Demo.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Demo {
+	
+	//Attribute
+	static ArrayList<Book> books = new ArrayList<Book>();
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner input = new Scanner(System.in);
+		String userChoice;
+		
+		try {
+			while(true) {
+				System.out.println("Do you want to add a book to the collection? Enter 'yes' to continue, or 'done' to exit.");
+				userChoice = input.next();
+				if(userChoice.equalsIgnoreCase("done")) {
+					break;
+				} 
+				else {
+					System.out.println("Enter the author's info.");
+					String author = input.nextLine();
+					String space = input.nextLine();
+					System.out.println("Enter the title.");
+					String title = input.nextLine();
+					System.out.println("Enter the book's isbn.");
+					long isbn = input.nextLong();
+					System.out.println("Enter the book's price.");
+					double price = input.nextDouble();
+					System.out.println("Is the book borrowed? true/false");
+					boolean isBorrowed = input.nextBoolean();
+					books.add(new Book(title, author, isbn, price, isBorrowed));		
+				}
+			}	
+		} catch(InputMismatchException e) {
+			System.out.println("Invalid input. The menu program will end. Goodbye!");
+		}
+	
+		System.out.println("The books in the collection are: " + books);
+		FileOutputStream fos = new FileOutputStream(
+				new File("C:\\Users\\ShafirFrazier\\OneDrive - Xpanxion\\Documents\\bookcollection.docx"));
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(books);
+		System.out.println("The books collection have been added into the file.");
+	}
+}
